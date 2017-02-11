b0bdcbef2166fe9c77d30fac96278eb0ddcee705
solomonnyefene <vansolo313@gmail.com>
2017-02-10 23:38:59 -1100

    My second commit on landing page

---
+++ assets/js/login/login.js
@@ -0,0 +1,18 @@
+
+  $( function() {
+    $( "#dialog" ).dialog({
+      autoOpen: false,
+      show: {
+        effect: "blind",
+        duration: 1000
+      },
+      hide: {
+        effect: "explode",
+        duration: 1000
+      }
+    });
+ 
+    $( "#opener" ).on( "click", function() {
+      $( "#dialog" ).dialog( "open" );
+    });
+  } )
--- assets/js/signup/signup.js
+++ assets/js/signup/signup.js
@@ -1,7 +1,4 @@
-
-   function overlay() {
+function overlay() {
 	el = document.getElementById("overlay");
 	el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
-       
-};
-
+}
